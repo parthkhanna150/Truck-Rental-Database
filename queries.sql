be51cf39aab519ee648d82d55c7177ba4bb6c250
parthkhanna150 <parth.khanna@mail.mcgill.ca>
2019-03-19 20:09:51 -0400

    cleaning

--- queries.sql
+++ queries.sql
@@ -2,14 +2,18 @@
 insert into customer values
 
 --2
-select DISTINCT firstname,lastname 
-from customer, booking 
+
+select DISTINCT firstname,lastname from customer, booking 
 where status='Cancelled' and customer.LICENCENUMBER=booking.LICENCENUMBER;
 -- if you delete, be sure to insert them
 delete from booking where status='Cancelled'
 
 --3--
-select truck.registration,size from truck,booking where truck.registration=booking.registration and notes='request drop off at airport' union select truck.registration,size from truck,booking where truck.registration=booking.registration and notes='request heated seats';
+select truck.registration,size from truck,booking 
+where truck.registration=booking.registration and notes='request drop off at airport' 
+union 
+select truck.registration,size from truck,booking 
+where truck.registration=booking.registration and notes='request heated seats';
 
 --4
 
